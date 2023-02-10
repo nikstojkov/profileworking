29933ab949a19b317ccfdef15668f90509dd5230
Nik Stojkov <nik.stojkov@gmail.com>
2023-02-10 15:21:26 +0000

    audio

--- control.js
+++ control.js
@@ -1,11 +1,10 @@
 let sound = false;
-const audio = new Audio('https://www.televisiontunes.com/uploads/audio/Benny%20Hill%20Show.mp3');
+// const audio = new Audio('https://www.televisiontunes.com/uploads/audio/Benny%20Hill%20Show.mp3');
 
 const runaway = (id) => {
   id.style.top = Math.round(Math.random() * 200) + 200 + "px";
   id.style.left = Math.round(Math.random() * 150) + 200 + "px";
   if (sound === false) {
-    audio.play();
-    sound = true;
+    new Audio('https://www.televisiontunes.com/uploads/audio/Benny%20Hill%20Show.mp3').play();
   }
 };
